23325ca0d724c936dd0967a89b2e9b033c6f877d
Alexey <you@example.com>
2020-05-18 19:18:32 +1000

    from unbutu

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'file:///C:/Users/Studio/AndroidStudioProjects/flutter_app/lib/app/Widget/myApp.dart';
+import 'package:flutterapp/app//Widget/myApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
